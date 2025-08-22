1d3b72cb5dcc2fa443e726632ca3d2318328934c
radunsb <radunsb@gmail.com>
2025-08-21 20:56:09 -0400

    Added get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,18 @@
+import os
+
+def get_file_content(working_directory, file_path):
+	full_path = os.path.normpath(os.path.join(working_directory, file_path))
+	if not full_path.startswith(working_directory):
+		return f"Error: Cannot read \"{file_path}\" as it is outside the permitted working directory"
+	if not os.path.isfile(full_path):
+		return f"Error: File not found or is not a regular file: \"{file_path}\""
+	try:
+		with open(full_path) as f:
+			content = f.read()
+			if(len(content) > 10000):
+				content = content[:10000] + f"[...File \"{file_path}\" truncated at 10000 character]"
+			return content
+	except Exception as e:
+		return f"Error: {e}"
+		
+
